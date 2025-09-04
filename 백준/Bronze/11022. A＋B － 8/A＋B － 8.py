70a59cdc76f143367eb37916d9b27a8a569d4b7e
MJ-dev-ai <mingyu.jang.dev@gmail.com>
2025-09-04 13:03:37 +0900

    [Bronze V] Title: A+B - 8, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+import sys
+
+t = int(input())
+for case in range(t):
+    inputs = sys.stdin.readline().rstrip().split()
+    a, b = int(inputs[0]), int(inputs[1])
+    print(f"Case #{case+1}: {a} + {b} = {a+b}")
